2de9f7a5831b280d0164a7d31c3b3dfb747b5f3b
Oever González <software@notengobattery.com>
2021-02-18 13:38:47 -0600

    add user_name as a valid parameter

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,32 +1,30 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  #def new
-  # super
-  #end
+  # def new
+  #  super
+  # end
 
-  #POST /resource
+  # POST /resource
   # def create
-  #   super
+  #  super
   # end
 
   # GET /resource/edit
   # def edit
-  #   super
+  #  super
   # end
 
   # PUT /resource
   # def update
-  #   super
+  #  super
   # end
 
   # DELETE /resource
   # def destroy
-  #   super
+  #  super
   # end
 
   # GET /resource/cancel
@@ -35,28 +33,28 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
   # def cancel
-  #   super
+  #  super
   # end
 
-  protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  end
+  # def configure_sign_up_params
+  #  devise_parameter_sanitizer.for(:sign_up)
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  end
+  # def configure_account_update_params
+  #  devise_parameter_sanitizer.permit(:account_update)
+  # end
 
   # The path used after sign up.
-  #def after_sign_up_path_for(resource)
-  # super(resource)
-  #end
+  # def after_sign_up_path_for(resource)
+  #  super(resource)
+  # end
 
   # The path used after sign up for inactive accounts.
-  #def after_inactive_sign_up_path_for(resource)
-  # super(resource)
-  #end
+  # def after_inactive_sign_up_path_for(resource)
+  #  super(resource)
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :user_name, presence: true, uniqueness: true, length: { minimum: 6, maximum: 10 }
-  validates :email, presence: true, uniqueness: true
+  validates :user_name, :email, presence: true, uniqueness: true
+  validates :user_name, length: { minimum: 6, maximum: 10 }
   validates :password, presence: true, length: { minimum: 6, maximum: 25 }
   has_many :posts
 end
